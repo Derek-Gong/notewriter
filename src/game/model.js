7804767b112c1c0b58f8b512afd0ab96f15de62d
Derek-Gong <derekgong.dg@gmail.com>
2020-09-04 15:41:40 +0900

    Add model.js

---
+++ src/game/model.js
@@ -0,0 +1,46 @@
+// {
+//     ticksPerQuarter: 220,
+//     totalTime: 2,
+//     timeSignatures: [
+//       {
+//         time: 0,
+//         numerator: 4,
+//         denominator: 4,
+//       }
+//     ],
+//     tempos: [
+//       {
+//         time: 0,
+//         qpm: 120
+//       }
+//     ],
+//   notes: [{
+//       pitch: 52,
+//       startTime: 0.0,
+//       endTime: 0.5
+//     },
+//     {
+//       pitch: 58,
+//       startTime: 0.5,
+//       endTime: 1.0
+//     },
+//     {
+//       pitch: 60,
+//       startTime: 1.0,
+//       endTime: 1.5
+//     }]
+//   }
+
+class Note {
+
+}
+
+class NoteSequence {
+
+}
+
+class NoteGenerator {
+    sample(noteSequence) {
+        //return a NoteSequence
+    }
+}
